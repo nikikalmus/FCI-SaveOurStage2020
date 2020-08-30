23b52d334ad3cab05381a9b3e33691c2ea039238
nikikalmus <56560540+nikikalmus@users.noreply.github.com>
2020-08-30 10:19:25 -0700

    Update Graph1_R.R

--- Graph1_R.R
+++ Graph1_R.R
@@ -7,7 +7,7 @@ rm(list = ls()) #clear your workspace
 
 # load data =======================================
 setwd("~/Documents/Improv/SOS/data_improv_sos")
-marriage <- read.csv("graph1_gay marriage.csv")
+sad <- read.csv("Graph1_sadGoogle.csv")
 google <- read.csv("Graph1_improvGoogle.csv")
 
 # prepare image packages ================================
@@ -19,9 +19,9 @@ library(magrittr) # For piping the logo
 
 library(zoo)
 google$Month.Year <- as.Date(as.yearmon(google$Month.Year))
-marriage$Month.Year <-as.Date(as.yearmon(marriage$Month.Year))
+sad$Month.Year <-as.Date(as.yearmon(sad$Month.Year))
 
-joined_df <- merge(google, marriage, by.x = "Month.Year", 
+joined_df <- merge(google, sad, by.x = "Month.Year", 
                    by.y = "Month.Year", all.x = FALSE, all.y = FALSE)
 
 
@@ -29,53 +29,57 @@ library(ggplot2)
 library(ggthemes)
 library(patchwork) # To display 2 charts together
 library(hrbrthemes)
+hrbrthemes::import_roboto_condensed()
+hrbrthemes::import_roboto_condensed()
 library(plotly)
+library(here) # For making the script run without a wd
 
-# themes you like = theme_gray() and theme_fivethirtyeight()
-plot1 <- ggplot(google,aes(x=Month.Year,y=Search.for.improv...California.)) +
+#separate plots: CA
+improvCA <- ggplot(google,aes(x=Month.Year,y=Search.for.improv...California.)) +
   geom_line() + 
   theme_ipsum()
 
-plot2 <- ggplot(marriage, aes(x=Month.Year,y=Should.be.legal))+
+sadCA <- ggplot(sad, aes(x=Month.Year,y=why.am.i.so.sad...California.))+
   geom_line() + 
   theme_ipsum()
 
-side_side <- plot1 + plot2
-side_side
+side_sideCA <- improvCA + sadCA
+side_sideCA
 
+# separate plots: USA
+improvUSA <- ggplot(google,aes(x=Month.Year,y=Search.for.improv...United.States.)) +
+  geom_line() + 
+  theme_ipsum()
 
-# Start with a usual ggplot2 call:
-ggplot(joined_df, aes(x=Month.Year, y=Should.be.legal)) +
-  # Custom the Y scales:
-  scale_y_continuous(
-    # Features of the first axis
-    name = "First Axis",
-    # Add a second axis and specify its features
-    sec.axis = sec_axis( trans=~.*1, name="Second Axis")
-  ) +
-   theme_fivethirtyeight()
+sadUSA <- ggplot(sad, aes(x=Month.Year,y=why.am.i.so.sad...United.States.))+
+  geom_line() + 
+  theme_ipsum()
+
+side_sideUSA <- improvUSA + sadUSA
+side_sideUSA
+
 
 ##########
 # A few constants
 googleColor <- "#69b3a2"
-marriageColor <- rgb(0.2, 0.6, 0.9, 1)
+sadColor <- rgb(0.2, 0.6, 0.9, 1)
 titleColor <- "#000000"
 
 just_graph <- ggplot(joined_df, aes(x=Month.Year)) +
-  geom_line( aes(y=Should.be.legal), size=1, color=googleColor, show.legend = TRUE) + 
-  geom_line( aes(y=Search.improv.comedy...United.States.), size=1, color=marriageColor) +
+  geom_line( aes(y=why.am.i.so.sad...United.States.), size=1, color=googleColor, show.legend = TRUE) + 
+  geom_line( aes(y=Search.improv.comedy...United.States.), size=1, color=sadColor) +
   scale_y_continuous( limits = c(0,100),
-    # Features of the first axis
-    name = "Public Approval ofLegalizing Gay Marriage",
-    # Add a second axis and specify its features
-    sec.axis = sec_axis(~.*1, name="Popularity of Googling 'improv comedy' in the USA")
+                      # Features of the first axis
+                      name = "Popularity of Googling 'why am I so sad",
+                      # Add a second axis and specify its features
+                      sec.axis = sec_axis(~.*1, name="Popularity of Googling 'improv comedy'")
   ) + 
   theme_grey()+
   theme(
     axis.title.y = element_text(color = googleColor, size=16, hjust = 1, margin = margin(t = 0, r = 20, b = 0, l = 0)),
     axis.text.y = element_text(color = googleColor, size=12),
-    axis.title.y.right = element_text(color = marriageColor, size=16, hjust = 1, margin = margin(t = 0, r = 0, b = 0, l = 20)),
-    axis.text.y.right = element_text(color = marriageColor),
+    axis.title.y.right = element_text(color = sadColor, size=16, hjust = 1, margin = margin(t = 0, r = 0, b = 0, l = 20)),
+    axis.text.y.right = element_text(color = sadColor),
     axis.title.x = element_text(color = titleColor, size=16, margin = margin(t = 20, r = 0, b = 0, l = 0)),
     legend.position="bottom"
   ) +
@@ -84,4 +88,40 @@ just_graph <- ggplot(joined_df, aes(x=Month.Year)) +
   xlab("Time") 
 
 just_graph
- 
+
+ggsave("graph1.png",  width = 20, height = 15, units = "cm")
+
+
+
+# Now call back the plot
+background <- image_read(paste0(here("/"), "graph1.png"))
+# And bring in a logo
+logo_raw <- image_read("https://media.giphy.com/media/23lFPv0BmMCz9OzwA1/giphy.gif", density = .5) 
+#logo_raw <- image_read("~/Documents/Improv/SOS/love_is_love.gif")
+#logo_raw <- image_scale(image_scale(logo_raw,"75"),"75")
+
+frames <- lapply(logo_raw, function(frame) {
+  image_composite(background, frame, offset = "+100+50")
+})
+
+animation <- image_animate(image_join(frames))
+
+
+image_write(animation, "~/Documents/Improv/SOS/Graph1.gif")
+
+
+## But let's break it down:
+plot1 <- ggplot(google,aes(x=Month.Year,y=Search.for.improv...California.)) +
+  geom_line() + 
+  theme_ipsum()
+
+plot2 <- ggplot(marriage, aes(x=Month.Year,y=Should.be.legal))+
+  geom_line(size=1, color=googleColor) + 
+  scale_y_continuous( limits = c(0,100),
+                      # Features of the first axis
+                      name = "Public Approval ofLegalizing Gay Marriage")
++ 
+  theme_ipsum()
+
+
+
